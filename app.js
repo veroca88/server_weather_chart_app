f0f565ea782d3894e87870d3e1dc285e0e0ccb26
veroca88 <veronicadonoso1506@gmail.com>
2020-11-23 10:43:24 -0600

    Add personal message using data

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-require('dotenv').config
+require('dotenv').config()
 const axios = require('axios').default
 
-
-const url = `http://api.weatherstack.com/current?access_key=${API_WEATHERSTACK_KEY}&query=Omaha`
+const url = `http://api.weatherstack.com/current?access_key=${process.env.API_WEATHERSTACK_KEY}&query=Omaha`
 
 axios({
     method: "GET",
     url: url,
-}).then(response => console.log("API RESPONSE", response.data)).catch(error => console.log("Some error happened", error))
+}).then(response => console.log(`Welcome to ${response.data.location.name}, the temperature is ${response.data.current.temperature} and it feels like ${response.data.current.feelslike}`)).catch(error => console.log("Some error happened", error))
