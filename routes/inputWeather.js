123c3c534824a8ade36fce2a72bddacca307b837
veroca88 <veronicadonoso1506@gmail.com>
2020-12-04 11:18:02 -0600

    Create inputWeather route, see input on backend

---
+++ routes/inputWeather.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const router = new Router();
+
+// const geoCode = require('./utils/geocode')
+// const weatherCode = require('./utils/weather')
+router.get('/', (req, res, next) => {
+    res.send('HELLOOOOOOOOOOOOO')
+})
+router.post('/', (req, res, next) => {
+    const { location } = req.body
+    console.log('============= post BACKEND', req.body, "location", location)
+    
+    // geoCode('Galapagos', (error, data) => {
+    //     if (error) return console.log('printing error', error)
+    // weatherCode(data.latitude, data.longitude, (error, weatherData) => {
+    //     if (error) return console.log('printing error', error)
+    //     console.log(data.location)
+    //     console.log(weatherData)
+    //     })
+    // })
+})
+
+module.exports = router
