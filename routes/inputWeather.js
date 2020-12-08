a410fcb2c61afa1ceeca9912558f1ca360e44b7d
veroca88 <veronicadonoso1506@gmail.com>
2020-12-08 15:12:32 -0600

    Incorporate tested node code to post request, works perfect

--- routes/inputWeather.js
+++ routes/inputWeather.js
@@ -1,23 +1,35 @@
 const { Router } = require("express");
 const router = new Router();
 
-// const geoCode = require('./utils/geocode')
-// const weatherCode = require('./utils/weather')
+const geoCode = require('../utils/geocode')
+const weatherCode = require('../utils/weather')
+
+
+
 router.get('/', (req, res, next) => {
     res.send('HELLOOOOOOOOOOOOO')
 })
 router.post('/', (req, res, next) => {
     const { location } = req.body
-    console.log('============= post BACKEND', req.body, "location", location)
+    // console.log('============= post BACKEND', req.body, "location", location)
+    geoCode(location, (error, data) => {
+        if (error) return console.log('printing error', error)
+        weatherCode(data.latitude, data.longitude, (error, weatherData) => {
+                if (error) return console.log('printing error', error)
+                // console.log(data.location)
+                // console.log(weatherData)
+                res.json(weatherData)
+                })
+    })
     
-    // geoCode('Galapagos', (error, data) => {
-    //     if (error) return console.log('printing error', error)
-    // weatherCode(data.latitude, data.longitude, (error, weatherData) => {
-    //     if (error) return console.log('printing error', error)
-    //     console.log(data.location)
-    //     console.log(weatherData)
-    //     })
-    // })
 })
 
+// geoCode('Galapagos', (error, data) => {
+//     if (error) return console.log('printing error', error)
+// weatherCode(data.latitude, data.longitude, (error, weatherData) => {
+//     if (error) return console.log('printing error', error)
+//     console.log(data.location)
+//     console.log(weatherData)
+//     })
+// })
 module.exports = router
