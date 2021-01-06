e461ea10874e76721ebf66e646a06bdac9ab4d6d
veroca88 <veronicadonoso1506@gmail.com>
2021-01-05 22:38:22 -0600

    Style main page

--- App.js
+++ App.js
@@ -5,28 +5,36 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const hbs = require('hbs')
 const cors = require('cors')
 
-const searchLocationRouter = require('./routes/inputWeather')
+
+const searchLocationRouter = require('./routes/inputWeather.routes')
+const index = require('./routes/index')
 
 const app = express()
 
-app.use(
-    cors({
-      origin: true,
-      credentials: true,
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    })
-  );
+// app.use(
+//     cors({
+//       origin: true,
+//       credentials: true,
+//       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     })
+//   );
 
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  app.set('view engine', 'hbs')
-  app.use(express.static(path.join(__dirname, "public")));
+
+  // Express View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
   
+
   app.use("/", searchLocationRouter);
+  app.use("/index", index);
   
   // catch 404 and forward to error handler
 app.use(function(req, res, next) {
