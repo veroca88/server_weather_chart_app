22a89e6aea04aa6ec433abbbade6cc91a560e4f1
veroca88 <veronicadonoso1506@gmail.com>
2021-01-14 15:57:31 -0600

    Render error if location does not exist

--- routes/inputWeather.routes.js
+++ routes/inputWeather.routes.js
@@ -7,13 +7,36 @@ const weatherCode = require('../utils/weather')
 
 
 let weatherMessage = ""
+
 router.get('/', (req, res ) => {
     res.render('weather')
 })
+
+router.get('/weather', (req, res) => {
+    let searchedLocation = req.query.address
+    geoCode(searchedLocation, (error, {location } = {}) => {
+        if (!location) {
+            res.render('weather',{
+            errorMessage: "You must provide a correct address"
+        })
+        return 
+    }
+        if (error) return console.log('printing error', error)
+            weatherCode(location, (error, weatherData) => {
+                if (error) return res.send('printing error', error)
+                res.render('weather', {
+                    response: weatherData,
+                    location,
+                    address: searchedLocation
+                })
+                })
+    })
+})
+
 router.post('/', (req, res) => {
     const { location } = req.body
         geoCode(location, (error, {location} = {}) => {
-            if (location === "Undefined" || location === undefined) return console.log('printing error', error)
+            if (!location) return console.log('printing error', error)
             if (error) return console.log('printing error', error)
             // weatherCode(longitude, latitude, (error, weatherData) => {
                 weatherCode(location, (error, weatherData) => {
